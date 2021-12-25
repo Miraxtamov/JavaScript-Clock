7e6b5f0a0081c64114354c88b4f5813e0dc02799
Miraxtamov <jmiraxtamov@gmail.com>
2021-12-25 16:09:25 +0500

    add clock performance

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function timeShower() {
+    var timeNow = new Date();
+
+    document.querySelector(".hours").textContent = timeNow.getHours();
+    document.querySelector(".minutes").textContent = timeNow.getMinutes();
+    document.querySelector(".seconds").textContent = timeNow.getSeconds();
+}
+
+timeShower();
+
+setInterval(function() {
+    timeShower();
+}, 1000);
